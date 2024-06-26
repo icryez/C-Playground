d32c5e7fa4b3b726606f02173bd55b2ca49cade6
icryez <chakradhar.dwivedi2000@gmail.com>
2024-06-26 20:55:56 +0000

    small change

--- program.cpp
+++ program.cpp
@@ -7,11 +7,11 @@ std::vector<int> twoSum(std::vector<int> &nums, int target) {
   std::unordered_map<int, int> map;
   for (int i = 0; i < nums.size(); i++) {
     if (map.count(target - nums[i]) && map[target - nums[i]] != i) {
-      return *new std::vector<int>{i, map[target - nums[i]]};
+      return {i, map[target - nums[i]]}; // can remove from new to {.
     }
     map[nums[i]] = i;
   }
-  return *new std::vector<int>{0, 0};
+  return {0, 0};
 }
 int main() {
   std::cout << "First program\n";
